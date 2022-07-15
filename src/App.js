7f092038e3e0021b42a0a555ca8c33c52cb8490b
Ahmed <ahmed.abdi.ali@se.experis.com>
2022-07-15 16:42:54 +0200

    added translation function logic

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 import Profile from './components/Profile/profilePage'
 import Login from "./views/Login";
-import Translate from "./views/Translate";
+import Translate from "./components/Translation/TranslationPage";
+// import Translate from "./views/Translate";
 function App() {
   return (
     <BrowserRouter>
--- src/components/Translation/translationPage.js
+++ src/components/Translation/translationPage.js
@@ -1,11 +1,74 @@
-import React from 'react';
+/**
+ * display the 10 last translation
+ * button to clear the translation(delete from the API)
+ * logout button should clear all the storage(redirect to Login page)
+ */
+ import React,{useState,useEffect} from 'react';
 
-function translationPage(props) {
-    return (
-        <div>
-            <h2>hello from translation</h2>
-        </div>
-    );
-}
-
-export default translationPage;
+//  import './translation.css'
+ 
+ 
+     const TranslationPage =props=> {
+         const [value, setValue]=useState('')
+         const [disabled, setDisabled]=useState([])
+ 
+         let tempArray = [];
+         let images =[
+             {src:'handSign'}
+         ]
+     
+ 
+         const handleChange =e=>{
+             setDisabled(false)
+             setValue(e.target.value)
+             console.log('value: '+value)
+         }
+ 
+         const handleSubmit =e=>{
+             e.preventDefault();
+             setDisabled(true)
+             if(value<1){
+                 alert('please enter a word')
+                 return;
+             }
+ 
+             for(let index = 0; index <value.length;index++){
+                 tempArray.push(value[index])
+                 // tempArray.push(`${images.src}`);
+         }
+             console.log('value stored : ',tempArray)
+         }
+ 
+ 
+ 
+         
+ 
+     return (
+         
+         <div>
+             <h2>welcome{}</h2>
+             <div>
+                 <form onSubmit={handleSubmit}>
+                     <input type="text" value={value} onChange={handleChange} placeholder="Enter text..."/>
+                     <button type="submit" disabled={disabled} className={disabled?"disabled":""}>click</button>
+                 </form>
+             </div>
+              {images.map((image,index)=>{
+                 return(
+                     <div key={index}>
+                         <ul>
+                             <img src={`${image.src}/${value}.png`}
+                                     alt=""
+                                     key={index}/>
+                         </ul>
+                         <div>
+                              //display here imageArrays
+                         </div>
+                 </div>
+                 )
+              })}
+         </div>
+     );
+ }
+ 
+ export default TranslationPage;
