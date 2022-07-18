b639aa1e330e3c0984dd1fc0d81786a84bcce878
Simon <simon.roshall@se.experis.com>
2022-07-18 14:53:52 +0200

    deleted deleteTranslation

--- src/api/translation.js
+++ src/api/translation.js
@@ -22,25 +22,3 @@ export const addTranslation = async (userid, translations) => {
         return [ error, null ]
     }
 }
-
-
-
-export const deleteTranslations = async (userid) => {
-       try {
-        const response = await fetch(`https://sros-noroff-api.herokuapp.com/translations/${userid}`, {
-            method: 'PATCH',
-            headers: createHeaders(),
-            body: JSON.stringify({
-                translations: []
-            })
-        })
-        if (!response.ok) {
-            throw new Error('Faild updating translations')
-        }
-        const result = await response.json()
-        return [ null, result ]
-
-       } catch (error) {
-            return [ error, null ]
-       } 
-}
